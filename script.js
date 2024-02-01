54ad52ea205dd7c620addfaf754c9bfc58a76513
OscarSanti29 <156258500+OscarSanti29@users.noreply.github.com>
2024-01-31 20:10:13 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+//Vault code is 10-40-39
+
+//Create equations that equal the vault codes above
+/* equations for vault numbers */
+let num1= 5 * 2;
+let num2= 80 / 2;
+let num3= 22 + 17;
+
+//create string to be able show message
+
+const secretmessage=
+`You have received this message because you have been chosen to open an important vault, Here is the secret combination: ${num1}, ${num2}, ${num3}`;
+console.log(secretmessage)
+
+//Create popup dialog for user to see message and vault numbers
+
+alert(secretmessage)
